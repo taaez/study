91e79a3b6c5c515663fd8ab442720a7203db686c
taez329 <ptj1541@gmail.com>
2024-04-15 11:41:35 +0000

    리스트 기반 스택

--- data_structure_algorithm/chapter02_Stack/LinkedListStack/LinkedListStack.h
+++ data_structure_algorithm/chapter02_Stack/LinkedListStack/LinkedListStack.h
@@ -17,17 +17,17 @@ typedef struct tagLinkedListStack
 	Node* Top;
 } LinkedListStack;
 
-void LLS_CreateStack(LinkedListStack** Stack);
-void LLS_DestroyStack(LinkedListStack* Stack);
+void LLS_CreateStack(LinkedListStack** Stack); // 스택 생성
+void LLS_DestroyStack(LinkedListStack* Stack); // 스택 해제
+ 
+Node* LLS_CreateNode(char* Data); // 스택에 넣을 노드 생성
+void LLS_DestroyNode(Node* _Node); // 노드 해제, 노드도 malloc으로 동적 할당 했다
 
-Node* LLS_CreateNode(char* Data);
-void LLS_DestroyNode(Node* _Node);
+void LLS_Push(LinkedListStack* Stack, Node* NewNode); // 스택에 삽입 push
+Node* LLS_Pop(LinkedListStack* Stack); // 스택에서 삭제하면서 반환
 
-void LLS_Push(LinkedListStack* Stack, Node* NewNode);
-Node* LLS_Pop(LinkedListStack* Stack);
-
-Node* LLS_Top(LinkedListStack* Stack);
-int LLS_GetSize(LinkedListStack* Stack);
+Node* LLS_Top(LinkedListStack* Stack); // 스택의 Top 반환
+int LLS_GetSize(LinkedListStack* Stack); // 스택 크기 구하기, 순차 탐색으로 리스트의 크기를 세서
 int LLS_IsEmpty(LinkedListStack* Stack);
 
 #endif
