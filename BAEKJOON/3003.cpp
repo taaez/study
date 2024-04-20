e62e2e353230669fea40be79ec0e768c74c5efca
taez329 <ptj1541@gmail.com>
2024-04-20 05:52:30 +0000

    24.04.19./Bronze5/https://www.acmicpc.net/problem/3003

--- BAEKJOON/3003.cpp
+++ BAEKJOON/3003.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	int chess[6];
+	int chess[6]; // 정수형 배열이므로 null 없어도 됨
 	
 	cin >> chess[0] >> chess[1] >> chess[2] >> chess[3] >> chess[4] >> chess[5];
 	cout << 1 - chess[0] <<' '<< 1-chess[1] <<' '<< 2-chess[2] <<' '<< 2-chess[3] <<' '<< 2-chess[4] <<' '<< 8-chess[5] << endl;
